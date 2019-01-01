6d8095aa2fe5de8b381c7bb6040f5f5b4fea7e0c
FlorentL00 <florent0710@gmail.com>
2019-01-01 22:34:55 +0100

    First commit of project

---
+++ src/com/FlorentL00/miam/Four.java
@@ -0,0 +1,14 @@
+package com.FlorentL00.miam;
+
+public class Four {
+
+    int puissance;
+    int capacite;
+
+    public void cuire(Aliment aliment){
+        System.out.println("Je cuit un aliment : " + aliment.nom);
+        System.out.println("avec ma capacité de " + capacite + " litres");
+        System.out.println("avec une puissance de " + puissance + " degrés");
+        aliment.estCuit = true;
+    }
+}
