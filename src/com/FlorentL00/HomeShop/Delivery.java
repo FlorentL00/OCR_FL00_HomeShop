b5eb856f13549df97d92cd68eec9e167d2efab59
FlorentL00 <florent0710@gmail.com>
2019-01-03 15:25:46 +0100

    OCR Chapter 7 - Interface Package HomeShop create Delivery system with interface and 4 delivery class

---
+++ src/com/FlorentL00/HomeShop/Delivery.java
@@ -0,0 +1,5 @@
+package com.FlorentL00.HomeShop;
+
+public interface Delivery {
+    public double getPrice();
+}
---
+++ src/com/FlorentL00/HomeShop/TakeAwayDelivery.java
@@ -0,0 +1,9 @@
+package com.FlorentL00.HomeShop;
+
+public class TakeAwayDelivery implements Delivery{
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+}
+
