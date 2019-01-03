b6f33fc36b7bae3dd57db68f9158704994d0b979
FlorentL00 <florent0710@gmail.com>
2019-01-03 17:57:03 +0100

    OCR Chapter 8 - Generate Bill

--- src/com/FlorentL00/HomeShop/Bill.java
+++ src/com/FlorentL00/HomeShop/Bill.java
@@ -1,10 +1,11 @@
 package com.FlorentL00.HomeShop;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Bill {
     private Customer customer;
-    private Map<Product, Integer> products;
+    private Map<Product, Integer> products = new HashMap<Product, Integer>();
     private Delivery delivery;
 
     public Bill(Customer customer, Delivery delivery) {
@@ -17,9 +18,10 @@ public class Bill {
      * @param product product to add
      * @param quantity quantity of the product
      */
-    public void addProduct(Product product, Integer quantity){
+    public void addProduct(Product product, int quantity) {
         this.products.put(product, quantity);
     }
+
 
     public Customer getCustomer() {
         return customer;
@@ -27,5 +29,49 @@ public class Bill {
 
     public Map<Product, Integer> getProducts() {
         return products;
+    }
+
+    /**
+     * Generate an output for the current Bill
+     * @param writer object in charge of writing
+     */
+    public void generate(Writer writer) {
+        writer.start();
+        writer.writeLine("HomeShop compagnie");
+        writer.writeLine("1 Place Charles de Gaulle, 75008 Paris");
+        writer.writeLine("");
+        writer.writeLine("Facture à l'attention de : ");
+        writer.writeLine(customer.getFullname());
+        writer.writeLine(customer.getAddress());
+        writer.writeLine("");
+        writer.writeLine("Mode de livraison : " + delivery.getInfo());
+        writer.writeLine("");
+        writer.writeLine("Produits : ");
+        writer.writeLine("-----------------------------------------------------");
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+            writer.writeLine(product.getName() + " - " + product.getPrice() + " - " + quantity + " unité(s)");
+            writer.writeLine(product.getDescription());
+            writer.writeLine("");
+        }
+        writer.writeLine("Livraison : " + delivery.getPrice());
+        writer.writeLine("-----------------------------------------------------");
+        writer.writeLine("Total : " + this.getTotal());
+        writer.stop();
+    }
+
+    /**
+     * get the total price for the current bill, including products and delivery cost
+     * @return total price
+     */
+    public double getTotal() {
+        double total = delivery.getPrice();
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+            total += product.getPrice() * quantity;
+        }
+        return total;
     }
 }
--- src/com/FlorentL00/HomeShop/Main.java
+++ src/com/FlorentL00/HomeShop/Main.java
@@ -1,6 +1,7 @@
 package com.FlorentL00.HomeShop;
 
 public class Main {
+
     public static void main(String[] args) {
         Product cafe = new Product("Philips HD7866/61", "Philips SENSEO Quadrante, Noir - 1 ou 2 tasses", 79.99);
         Television tv = new Television("TV Samsung UE49MU6292", "Smart TV LED incurvée 49\"", 599, 49, "LED");
@@ -8,10 +9,12 @@ public class Main {
 
         Customer customer = new Customer("Juste Leblanc", "19 rue Germain Pilon, Paris");
 
-        Bill bill = new Bill(customer);
+        Bill bill = new Bill(customer, new RelayDelivery(27));
         bill.addProduct(cafe, 1);
         bill.addProduct(tv, 1);
         bill.addProduct(fridge, 1);
+
+        bill.generate(new FileWritter("facture leblanc"));
     }
 
 }
